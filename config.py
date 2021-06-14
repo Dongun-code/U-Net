66af8d33a1eb0125dd076a0e4b3a46e0fb606e68
Dongun-code <dongun04@outlook.com>
2021-06-14 22:59:43 +0900

    fix data preprocess process

--- config.py
+++ config.py
@@ -4,20 +4,26 @@ import os.path as op
 class Config:
     RESULT_ROOT = "/home/milab/machine_ws/Semantic Segmentation/Dataset"
     TFRECORD = op.join(RESULT_ROOT, "tfrecord")
-    CITY_DATA = op.join(RESULT_ROOT, "cityscapes_data")
+    CITY_DATA = op.join(RESULT_ROOT,"leftImg8bit")
+    CITY_DATA_LABEL = op.join(RESULT_ROOT,"gt","gtFine")
+    USE_CATEGORY = ['car', 'road', 'person']
+    COLOR = {'car' : 15,
+            'road' : 25,
+            'traffic sign' : 35,
+            'sidewalk' : 45,
+            'terrain' : 55 ,
+            'person' : 65}
 
     class Tfrecord:
         pass
 
     class Model:
-        colors = [(255,0,0), (0,255,0), (0,0,255),
-                (255,255,0), (255,0,255), (0,255,255),
-                (255,255,255), (200,50,0),(50,200,0),
-                (50,0,200), (200,200,50), (0,50,200),
-                (0,200,50), (0,0,0)]
-        input_shape = (256, 256, 3)
+
+        input_shape = (256, 512)
         model = "unet"
         batch_size = 2
+
+    
     
 
 
--- data.py
+++ data.py
@@ -7,7 +7,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import cv2
 import pandas as pd
-from sklearn.cluster import KMeans
+# from sklearn.cluster import KMeans
 
 class data_factory:
     def __init__(self, dataset):
@@ -16,78 +16,49 @@ class data_factory:
 
     
 class CitySpace():
-    def kmeans(self, img_list):
-        img_list.sort()
-        mask_list = []
-        for name in img_list:
-            img = Image.open(name)
-            img = np.array(img, dtype=np.float32)
-            img = self.cut_img(img)
-            mask = img[:,256:, :]
-            mask = mask / 255
-            mask_list.append(mask.reshape(mask.shape[0]*mask.shape[1], 3))
+    def __init__(self):
+        self.data_path = cfg.CITY_DATA
+        self.label_path = cfg.CITY_DATA_LABEL
+        self.obj_color = cfg.COLOR
+        self.input_shape = cfg.Model.input_shape
 
-        mask_list = np.array(mask_list)
-        mask = mask_list.reshape((-1, 3))
-        kmeans = KMeans(10)
-        kmeans.fit(mask)
-
-        return kmeans
-
-    def collect_img(self, img_list):
-        img_list.sort()
-        climg_list = []
-        mask_list = []
-        
-        for name in img_list:
-            img = Image.open(name)
-            img = np.array(img, dtype=np.float32)
-            img = self.cut_img(img)
-            climg = img[:, :256, :]
-            mask = img[:,256:, :]
-
-            climg = climg / 255
-            mask = mask / 255
-            climg_list.append(climg)
-            mask_list.append(mask)
-
-        climg_list = np.array(climg_list)
-        mask_list = np.array(mask_list)
-
-        return climg_list, mask_list
-
-    def cut_img(self, img):
-        img = img[:-56, :, :]
-        return img
-
-    def colorCluster(self, img):
+    def img_process(self, img):
+        pass
+    
+    def label_process(self, label):
         pass
 
+    def get_data(self, path_, split):
+        path = op.join(path_, split, "*")
+        folder = glob.glob(path)
+        folder.sort()
+        file_list = []
+        for fold in folder:
+            if "leftImg8bit" in path_:
+                path = op.join(fold,"*.png")
+            elif "gtFine" in path_:
+                path = op.join(fold, "*.json")
+            data_list = glob.glob(path)
+            data_list.sort()
+            file_list.extend(data_list)
+
+        print("file shape : ", np.array(file_list).shape)
+        if "leftImg8bit" in path_:
+            self.img_process(file_list)
+        elif "gtFine" in path_:
+            self.label_process(file_list)
+
+
+        return file_list
+        
 
     def __call__(self, path):
 
-        train_path = op.join(cfg.CITY_DATA, "train","*.jpg")
-        val_path = op.join(cfg.CITY_DATA, "valid","*.jpg")
+        train = self.get_data(self.data_path, "train")
 
-        train_data = glob.glob(train_path)
-        val_data = glob.glob(val_path)
-        train_data.sort()
 
-        kmean = self.kmeans(train_data[:30])
-        train_img, train_mask = self.collect_img(train_data)
-        # val_img, train_mask = self.collect_img(val_data)
 
-        print(train_mask[20].reshape(-1, 3).shape)
-        result = kmean.predict(train_mask[50].reshape(-1, 3))
-        print(result.shape)
-        print(result)
-        result = result.reshape((200,256))
-        print(result)
 
-        fig, axes = plt.subplots(1, 2, figsize=(15, 5))
-        axes[0].imshow(result)
-        axes[1].imshow(train_mask[50])
-        plt.show()
 
 
 
